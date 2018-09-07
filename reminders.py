d52858ae0c5dd3a7146ed2bdf21da397906bab98
Matteo Franzil <32071488+mfranzil@users.noreply.github.com>
2018-09-07 08:55:25 +0200

    Time-zone bug fixed

--- reminders.py
+++ reminders.py
@@ -18,19 +18,19 @@ def remind():
 
 
 def remind_driver(bot, chat_id):
-    """Questo comando verrà eseguito alle 00:30 di ogni giorno feriale"""
+    """Questo comando verrà eseguito alle 23:30 di ogni giorno feriale"""
     today = datetime.datetime.today().weekday()
-    if 0 <= today <= 4:
+    if 0 <= (today + 1) % 7 <= 4:
         heading_sent = False
 
         for direction in secret_data.groups:
-            if str(chat_id) in secret_data.groups[direction][common.today()]:
+            if str(chat_id) in secret_data.groups[direction][common.tomorrow()]:
                 # Mando il messaggio iniziale una sola volta
                 if not heading_sent:
                     bot.send_message(chat_id=chat_id, text="Sommario dei viaggi per domani:")
                     heading_sent = True
 
-                trip = secret_data.groups[direction][common.today()][str(chat_id)]
+                trip = secret_data.groups[direction][common.tomorrow()][str(chat_id)]
                 bot.send_message(chat_id=chat_id,
                                  text="Viaggio " + common.direction_to_name(direction) + " - " + trip["Time"] + "\n\n" +
                                       "Permanentemente: " + ",".join(trip["Permanent"]) + "\n" +
@@ -38,15 +38,15 @@ def remind_driver(bot, chat_id):
 
 
 def remind_user(bot, chat_id):
-    """Questo comando verrà eseguito alle 00:30 di ogni giorno feriale"""
+    """Questo comando verrà eseguito alle 23:30 di ogni giorno feriale"""
     bookings = common.search_by_booking(str(chat_id))
 
     today = datetime.datetime.today().weekday()
-    if 0 <= today <= 4:
+    if 0 <= (today + 1) % 7 <= 4:
         for item in bookings:
             direction, day, driver, mode = item
-            if day == common.today():
+            if day == common.tomorrow():
                 bot.send_message(chat_id=chat_id,
-                                 text="REMINDER: Oggi hai un viaggio " + common.direction_to_name(direction) +
+                                 text="REMINDER: Domani hai un viaggio " + common.direction_to_name(direction) +
                                       " alle ore " + common.get_trip_time(driver, day, direction) + " " +
                                       common.direction_to_name(direction) + " con " + str(driver))
